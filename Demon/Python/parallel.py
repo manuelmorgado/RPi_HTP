119367435e8336c96a1e56e1d78c0d2fb9345ab2
manuelmorgado <manuelmorgadov@gmail.com>
2016-08-08 16:31:40 +0200

    Prueba 3

--- Demon/Python/parallel.py
+++ Demon/Python/parallel.py
@@ -8,24 +8,36 @@
 #     print(p.map(f, [1, 2, 3]))
 
 
-from multiprocessing import Process
-import os
+# from multiprocessing import Process
+# import os
+# 
+# def info(title):
+#     print title
+#     print 'module name:', __name__
+#     if hasattr(os, 'getppid'):  # only available on Unix
+#         print 'parent process:', os.getppid()
+#     print 'process id:', os.getpid()
+# 
+# def f(name):
+#     info('function f')
+#     print 'hello', name
+# 
+# if __name__ == '__main__':
+#     info('main line')
+#     p = Process(target=f, args=('bob',))
+#     p.start()
+#     p.join()
 
-def info(title):
-    print title
-    print 'module name:', __name__
-    if hasattr(os, 'getppid'):  # only available on Unix
-        print 'parent process:', os.getppid()
-    print 'process id:', os.getpid()
 
-def f(name):
-    info('function f')
-    print 'hello', name
+from multiprocessing import Process, Pipe
+
+def f(conn):
+    conn.send([42, None, 'hello'])
+    conn.close()
 
 if __name__ == '__main__':
-    info('main line')
-    p = Process(target=f, args=('bob',))
+    parent_conn, child_conn = Pipe()
+    p = Process(target=f, args=(child_conn,))
     p.start()
+    print parent_conn.recv()   # prints "[42, None, 'hello']"
     p.join()
-    
-    
