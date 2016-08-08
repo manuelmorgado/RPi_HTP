c2fab989840e7d1e641203dc539d68d09663061a
manuelmorgado <manuelmorgadov@gmail.com>
2016-08-08 17:27:01 +0200

    Infinite loop

--- BMP180/Python/main.py
+++ BMP180/Python/main.py
@@ -1,7 +1,18 @@
-key = raw_input();
+# key = raw_input();
+# 
+# while bool(key):
+# 	execfile("bmp180.py")
+# 
+# 	if bool(key) != True:
+# 		break
+import sys, select, os
 
-while bool(key):
-	execfile("bmp180.py")
-
-	if bool(key) != True:
-		break
+i = 0
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    execfile("bmp180.py")
+    print i
+    if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        line = raw_input()
+        break
+    i += 1
