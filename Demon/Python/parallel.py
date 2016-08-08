3dbdf9084b799b2e9b3292e16fb73f76d503c21f
manuelmorgado <manuelmorgadov@gmail.com>
2016-08-08 16:27:41 +0200

    Prueba 2

--- Demon/Python/parallel.py
+++ Demon/Python/parallel.py
@@ -1,8 +1,31 @@
-from multiprocessing import Pool
+# from multiprocessing import Pool
+# 
+# def f(x):
+#     return x*x
+# 
+# if __name__ == '__main__':
+#     p = Pool(1)
+#     print(p.map(f, [1, 2, 3]))
 
-def f(x):
-    return x*x
+
+from multiprocessing import Process
+import os
+
+def info(title):
+    print title
+    print 'module name:', __name__
+    if hasattr(os, 'getppid'):  # only available on Unix
+        print 'parent process:', os.getppid()
+    print 'process id:', os.getpid()
+
+def f(name):
+    info('function f')
+    print 'hello', name
 
 if __name__ == '__main__':
-    p = Pool(1)
-    print(p.map(f, [1, 2, 3]))
+    info('main line')
+    p = Process(target=f, args=('bob',))
+    p.start()
+    p.join()
+    
+    
