c02fe73caaa21ff95724dcf3c96efb82115b1b8b
Manuel Morgado <manuelmorgadov@gmail.com>
2016-08-18 09:34:45 +0000

    works

--- SHT21/Python/sht21.py
+++ SHT21/Python/sht21.py
@@ -48,23 +48,28 @@ class sht21:
         self.i2c.write([0xFE])  # execute Softreset Command  (default T=14Bit RH=12)
         time.sleep(0.050)
 
-    def read_temperature(self):
+    def read_temperature(self):#, dev=1, scl=3, sda=2):
         """ Temperature measurement (no hold master), blocking for ~ 88ms !!! """
-        self.i2c.write([0xF3])
-        time.sleep(0.036)  # wait, typ=66ms, max=85ms @ 14Bit resolution
+        #self.i2c.open(dev, scl, sda)
+	self.i2c.write([0xF3]) # Trigger T measurement
+        time.sleep(0.066)  # Waiting time, typ=66ms, max=85ms @ 14Bit resolution
         data = self.i2c.read(3)
-        if (self._check_crc(data, 2)):
+
+	if (self._check_crc(data, 2)):
             t = ((data[0] << 8) + data[1]) & 0xFFFC  # set status bits to zero
             t = -46.82 + ((t * 175.72) / 65536)  # T = 46.82 + (175.72 * ST/2^16 )
-            return round(t, 1)
+            #self.i2c.close()
+	    return round(t, 1)
         else:
+	    #self.i2c.close()
             return None
 
     def read_humidity(self):
         """ RH measurement (no hold master), blocking for ~ 32ms !!! """
         self.i2c.write([0xF5])  # Trigger RH measurement (no hold master)
-        time.sleep(0.03)  # wait, typ=22ms, max=29ms @ 12Bit resolution
+        time.sleep(0.03)  # Waiting time, typ=22ms, max=29ms @ 12Bit resolution
         data = self.i2c.read(3)
+
         if (self._check_crc(data, 2)):
             rh = ((data[0] << 8) + data[1]) & 0xFFFC  # zero the status bits
             rh = -6 + ((125 * rh) / 65536)
@@ -98,7 +103,7 @@ if __name__ == "__main__":
     SHT21 = sht21()
     while True:
         try:
-            ############################################################################################################
+           ##########################################################################################
             # Example 1 Using the I2C Driver
             ############################################################################################################
 
@@ -120,12 +125,13 @@ if __name__ == "__main__":
             #(t1, rh1) = SHT21.measure(None,7,11)  # Use GPIOs SCL=3, SDA=2
             #print("%s°C\t%s%%\t%s°C\t%s%%" % (t0,rh0,t1,rh1))
 	    (t0 , rh0) = SHT21.measure(None,3,2)
-	    time.sleep(0.06)	
-	    (t1 , rh1) = SHT21.measure(None, 3,2)
+	    time.sleep(0.1)	
+	    #(t1 , rh1) = SHT21.measure(None, 3,2)
 	    #print ("%s C&C\tC\t%s%%\t%s  C\t%s%%" % (t0,rh0,t1,rh1))
-	    t3 = SHT21.read_temperature()
-	    print t3            
- 	    print t0, rh0, t1, rh1
+	    #t3 = SHT21.read_temperature(None, 3,2)
+	    #time.sleep(0.1)
+	    print t0            
+ 	    #print t0, rh0, t1, rh1
 	    ############################################################################################################
         except:
             print("sht21 I/O Error")
